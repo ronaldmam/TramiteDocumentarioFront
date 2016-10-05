9bad358277fa06cf848b81a4de58aa781a32de16
RONALD MAMANI NINAJA <RONALD MAMANI NINAJA>
2016-10-05 16:46:26 -0500

    primera actualizacion

---
+++ node_modules/ag-grid/dist/lib/entities/originalColumnGroupChild.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for ag-grid v6.1.0
+// Project: http://www.ag-grid.com/
+// Definitions by: Niall Crosby <https://github.com/ceolter/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+export interface OriginalColumnGroupChild {
+    getColumnGroupShow(): string;
+    getId(): string;
+}
